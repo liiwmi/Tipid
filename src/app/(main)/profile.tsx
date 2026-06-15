e66ba6d0d1ad2220383c6636fe963eb9cb7eea8f
ChelseyRei <kauchinrei@gmail.com>
2026-06-15 09:40:12 +0800

    Update profile.tsx

--- src/app/(main)/profile.tsx
+++ src/app/(main)/profile.tsx
@@ -63,7 +63,7 @@ export default function ProfileScreen() {
       return;
     }
     const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ["images"],
       allowsEditing: true,
       aspect: [1, 1],
       quality: 0.7,
